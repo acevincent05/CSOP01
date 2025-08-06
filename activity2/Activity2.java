a25647f5065b8849243c8abc431c297deeb5779e
Ace Gamitin <gamitinacevincent@gmail.com>
2025-08-06 20:07:54 +0800

    Add files via upload

---
+++ activity2/Activity2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package activity2;
+
+/**
+ *
+ * @author Student
+ */
+public class Activity2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("********************");
+        System.out.println("JAVA");
+        System.out.println("      LANGAUGE");
+        System.out.println("********************");
+    }
+    
+}
