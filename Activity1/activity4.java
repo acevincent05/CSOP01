649f3007750d992ff52520d02d4d0ae9de9707f2
Ace Gamitin <gamitinacevincent@gmail.com>
2025-08-06 20:09:45 +0800

    Add files via upload

---
+++ Activity1/activity4.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package activity2;
+
+/**
+ *
+ * @author Student
+ */
+public class activity4 {
+    public static void main(String[] args) {
+        double capuccino_price, mocha_price, vanilla_price;
+        
+        capuccino_price = 95;
+        mocha_price = 90;
+        vanilla_price = 105;
+        
+        
+        System.out.print("JAVA CAFE");
+        System.out.print("Capuccino- "+capuccino_price+"\n");
+        System.out.print("Mocha- "+mocha_price+"\n");
+        System.out.print("Vanilaa- "+vanilla_price+"\n");
+    }
+}
---
+++ Activity1/activity6.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package activity2;
+
+/**
+ *
+ * @author Student
+ */
+public class activity6 {
+    public static void main(String[] args) {
+        System.out.println("(7%3+50-3*3)%(5+10*2-6/3)= "+(7%3+50-3*3)%(5+10*2-6/3));
+        System.out.println("45/5-7+5*5-(120-10*4+10%4= "+(45/5-7+5*5-(120-10*4+10%4)));
+        System.out.println("(5*5+10-20/5)-4+5*10-20%3= "+((5*5+10-20/5)-4+5*10-20%3));
+    }
+}
