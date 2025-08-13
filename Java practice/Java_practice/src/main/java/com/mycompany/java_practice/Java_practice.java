16f6af51e1a9e609a4d59fb2b47b7020287cb66a
Ace Gamitin <gamitinacevincent@gmail.com>
2025-08-13 11:13:04 +0800

    Add files via upload

---
+++ Java practice/Java_practice/src/main/java/com/mycompany/java_practice/Java_practice.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java_practice;
+
+/**
+ *
+ * @author Ace Gamitin
+ */
+import javax.swing.JOptionPane;
+public class Java_practice {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "test", "Input", JOptionPane.INFORMATION_MESSAGE);
+                
+    }
+}
